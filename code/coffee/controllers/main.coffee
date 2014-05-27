51c92d0a7e9c2558af0e36b2a5d0299ea930d83a
Curtis M. Humphrey, Ph.D <curtis@createdwithflair.com>
2014-05-26 21:15:01 -0500

    Updated to end of class 03 by adding in some coffeescript and knockout bindings

--- code/coffee/controllers/main.coffee
+++ code/coffee/controllers/main.coffee
@@ -2,9 +2,43 @@ define (require) ->
   ko = require 'knockout'
   $ = require "jquery"
 
+  ToHMM = (minutes) ->
+    h = Math.floor minutes / 60
+    m = minutes % 60
+    m = "0"+m if m < 10
+    ""+h+":"+m
+
+
   class Main_VM
     constructor: ->
-      @title = ko.observable "Hello"
+      @washer_options = ko.observableArray []
+      @dryer_options  = ko.observableArray []
+      @snooze_options = ko.observableArray []
 
-      @title.subscribe (new_value) -> console.log "Value changed to "+new_value
+      options = []
+      for minutes in [10..4*50] by 5
+        options.push 
+          name: ToHMM minutes
+          minutes: minutes
+      @washer_options options
+
+      options = []
+      for minutes in [10..4*50] by 5
+        options.push 
+          name: ToHMM minutes
+          minutes: minutes
+      @dryer_options options
+
+      options = []
+      for minutes in [1..30]
+        options.push 
+          name: ToHMM minutes
+          minutes: minutes
+      @snooze_options options
+
+      @washer_time = ko.observable 40
+      @dryer_time  = ko.observable 60
+      @snooze_time = ko.observable 10
+
+      @washer_time.subscribe (nV) -> console.log nV
       return
--- code/js/controllers/main.js
+++ code/js/controllers/main.js
@@ -1,13 +1,52 @@
 (function() {
   define(function(require) {
-    var $, Main_VM, ko;
+    var $, Main_VM, ToHMM, ko;
     ko = require('knockout');
     $ = require("jquery");
+    ToHMM = function(minutes) {
+      var h, m;
+      h = Math.floor(minutes / 60);
+      m = minutes % 60;
+      if (m < 10) {
+        m = "0" + m;
+      }
+      return "" + h + ":" + m;
+    };
     return Main_VM = (function() {
       function Main_VM() {
-        this.title = ko.observable("Hello");
-        this.title.subscribe(function(new_value) {
-          return console.log("Value changed to " + new_value);
+        var minutes, options, _i, _j, _k, _ref, _ref1;
+        this.washer_options = ko.observableArray([]);
+        this.dryer_options = ko.observableArray([]);
+        this.snooze_options = ko.observableArray([]);
+        options = [];
+        for (minutes = _i = 10, _ref = 4 * 50; _i <= _ref; minutes = _i += 5) {
+          options.push({
+            name: ToHMM(minutes),
+            minutes: minutes
+          });
+        }
+        this.washer_options(options);
+        options = [];
+        for (minutes = _j = 10, _ref1 = 4 * 50; _j <= _ref1; minutes = _j += 5) {
+          options.push({
+            name: ToHMM(minutes),
+            minutes: minutes
+          });
+        }
+        this.dryer_options(options);
+        options = [];
+        for (minutes = _k = 1; _k <= 30; minutes = ++_k) {
+          options.push({
+            name: ToHMM(minutes),
+            minutes: minutes
+          });
+        }
+        this.snooze_options(options);
+        this.washer_time = ko.observable(40);
+        this.dryer_time = ko.observable(60);
+        this.snooze_time = ko.observable(10);
+        this.washer_time.subscribe(function(nV) {
+          return console.log(nV);
         });
         return;
       }
