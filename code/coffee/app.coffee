62bba978477d1ba7bfb1db2956703c9bf8ef606e
Curtis M. Humphrey, Ph.D <curtis@createdwithflair.com>
2014-05-26 21:48:05 -0500

    Added hammer for touch

--- code/coffee/app.coffee
+++ code/coffee/app.coffee
@@ -5,6 +5,8 @@ requirejs.config
     knockout_amd: "../../bower_components/knockout-amd-helpers/build/knockout-amd-helpers"
     jquery: "../../bower_components/jquery/dist/jquery"
     bootstrap: "../../bower_components/bootstrap/dist/js/bootstrap"
+    hammer: "../../bower_components/hammerjs/hammer"
+    knockout_hammer: "../../bower_components/knockout-hammer/knockout-hammer"
 
   baseUrl: '/code/js'
   shim:
@@ -16,7 +18,8 @@ define [
   'knockout'
   'knockout_amd'
   'bootstrap'
-], (ko, amd, bootstrap) ->
+  'knockout_hammer'
+], (ko, amd, bootstrap, hammer) ->
 
   ko.amdTemplateEngine.defaultPath = "/ko_templates";
   ko.amdTemplateEngine.defaultSuffix = ".html";
--- code/js/app.js
+++ code/js/app.js
@@ -5,7 +5,9 @@
       knockout: "../../bower_components/knockout/build/output/knockout-latest.debug",
       knockout_amd: "../../bower_components/knockout-amd-helpers/build/knockout-amd-helpers",
       jquery: "../../bower_components/jquery/dist/jquery",
-      bootstrap: "../../bower_components/bootstrap/dist/js/bootstrap"
+      bootstrap: "../../bower_components/bootstrap/dist/js/bootstrap",
+      hammer: "../../bower_components/hammerjs/hammer",
+      knockout_hammer: "../../bower_components/knockout-hammer/knockout-hammer"
     },
     baseUrl: '/code/js',
     shim: {
@@ -16,7 +18,7 @@
     }
   });
 
-  define(['knockout', 'knockout_amd', 'bootstrap'], function(ko, amd, bootstrap) {
+  define(['knockout', 'knockout_amd', 'bootstrap', 'knockout_hammer'], function(ko, amd, bootstrap, hammer) {
     ko.amdTemplateEngine.defaultPath = "/ko_templates";
     ko.amdTemplateEngine.defaultSuffix = ".html";
     ko.amdTemplateEngine.defaultRequireTextPluginName = "text";
