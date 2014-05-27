f8c027f718e163525ec13616191610100bb6d2c5
Curtis M. Humphrey, Ph.D <curtis@createdwithflair.com>
2014-05-26 21:48:05 -0500

    Added hammer for touch

--- code/js/app.js
+++ code/js/app.js
@@ -5,10 +5,16 @@
       knockout: "../../bower_components/knockout/build/output/knockout-latest.debug",
       knockout_amd: "../../bower_components/knockout-amd-helpers/build/knockout-amd-helpers",
       jquery: "../../bower_components/jquery/dist/jquery",
-      bootstrap: "../../bower_components/bootstrap/dist/js/bootstrap"
+      bootstrap: "../../bower_components/bootstrap/dist/js/bootstrap",
+      hammer: "../../bower_components/hammerjs/hammer",
+      knockout_hammer: "../../bower_components/knockout-hammer/knockout-hammer"
     },
     baseUrl: '/code/js',
     shim: {
+      'knockout_hammer': {
+        deps: ['hammer', 'knockout'],
+        exports: 'hammer'
+      },
       'bootstrap': {
         deps: ['jquery'],
         exports: 'jQuery'
@@ -16,7 +22,7 @@
     }
   });
 
-  define(['knockout', 'knockout_amd', 'bootstrap'], function(ko, amd, bootstrap) {
+  define(['knockout', 'knockout_amd', 'bootstrap', 'knockout_hammer'], function(ko, amd, bootstrap, hammer) {
     ko.amdTemplateEngine.defaultPath = "/ko_templates";
     ko.amdTemplateEngine.defaultSuffix = ".html";
     ko.amdTemplateEngine.defaultRequireTextPluginName = "text";
