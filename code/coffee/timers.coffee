1b50e096a6f263be1cd38ee740040ff53873ed0d
Curtis M. Humphrey, Ph.D <curtis@createdwithflair.com>
2014-06-15 16:55:24 -0500

    Material covered in Class 06

--- code/coffee/timers.coffee
+++ code/coffee/timers.coffee
@@ -1,8 +1,33 @@
 define (require) ->
   ko = require 'knockout'
   $ = require "jquery"
+  require 'kox_morf'
 
   class Timers_VM
     constructor: ->
       @washer_time = ko.observable "1:05:00"
       @dryer_time = ko.observable "1:05:00"
+
+      @washer = []
+      @washer.pos = ko.observable true
+      @washer.on = ko.observable false
+      @washer.effect = ko.computed =>
+        unless @washer.on()
+          animate =
+            css:
+              '-webkit-transform': 'rotate(0deg)'
+            parameters:
+              duration: '200ms'
+        else
+          animate =
+            css:
+              '-webkit-transform': if @washer.pos() then 'rotate(5deg)' else 'rotate(-5deg)'
+            parameters:
+              duration: '300ms'
+              timingfunction: 'ease-out'
+              callback: =>
+                @washer.pos !@washer.pos()
+
+    ##Event Bindings
+    Tap_Washer: (d, e) =>
+      @washer.on !@washer.on()
--- code/js/timers.js
+++ code/js/timers.js
@@ -1,13 +1,52 @@
 (function() {
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
   define(function(require) {
     var $, Timers_VM, ko;
     ko = require('knockout');
     $ = require("jquery");
+    require('kox_morf');
     return Timers_VM = (function() {
       function Timers_VM() {
+        this.Tap_Washer = __bind(this.Tap_Washer, this);
         this.washer_time = ko.observable("1:05:00");
         this.dryer_time = ko.observable("1:05:00");
+        this.washer = [];
+        this.washer.pos = ko.observable(true);
+        this.washer.on = ko.observable(false);
+        this.washer.effect = ko.computed((function(_this) {
+          return function() {
+            var animate;
+            if (!_this.washer.on()) {
+              return animate = {
+                css: {
+                  '-webkit-transform': 'rotate(0deg)'
+                },
+                parameters: {
+                  duration: '200ms'
+                }
+              };
+            } else {
+              return animate = {
+                css: {
+                  '-webkit-transform': _this.washer.pos() ? 'rotate(5deg)' : 'rotate(-5deg)'
+                },
+                parameters: {
+                  duration: '300ms',
+                  timingfunction: 'ease-out',
+                  callback: function() {
+                    return _this.washer.pos(!_this.washer.pos());
+                  }
+                }
+              };
+            }
+          };
+        })(this));
       }
+
+      Timers_VM.prototype.Tap_Washer = function(d, e) {
+        return this.washer.on(!this.washer.on());
+      };
 
       return Timers_VM;
 
